a8e5c3d49d7eca9927fbbf858a3dfc79973f88e9
Rogerio Ribeiro da Cruz <rogeriorc@gmail.com>
2016-10-26 16:02:43 -0200

    .

--- tds.js
+++ tds.js
@@ -3,16 +3,24 @@
 let Q = require('q'),
 	path = require('path'),
 	os = require('os'),
-	spawn = require('child_process').spawn;
+	fs = require('fs'),
+	spawn = require('child_process').spawn,
+	execSync = require('child_process').execSync;
 
 class TDS {
 
 	constructor(directory) {
+		this.findTdsHome();
+		this.findJava();
+		this.findLauncher();
+		this.findVersion();
+
 		this.cwd = path.resolve(directory || process.env.TDS_HOME || process.cwd());
 		this.cwd = path.normalize(this.cwd + path.sep);
 
+		console.log("TDS Version: " + this.version);
+
 		//this.command = (os.platform() === 'win32' ? 'eclipsec.exe' : 'developerStudio');
-		this.command = path.join('jre', 'bin', (os.platform() === 'win32' ? 'java.exe' : 'java'));
 		//Mac?
 		//Eclipse.app/Contents/MacOS/eclipse
 	}
@@ -23,7 +31,9 @@ class TDS {
 		var args = this._get_args('compile', options),
 			proc = null;
 
-		proc = spawn(this.command, args, {
+		console.log("COMMAND:\n" + this.java + ' ' + args.join(' '));
+
+		proc = spawn(this.java, args, {
 			cwd: this.cwd,
 			stdio: ['ignore', 'pipe', 'pipe']
 		});
@@ -61,13 +71,23 @@ class TDS {
 	}
 
 	_get_args(target, options) {
-		var args = [];
-		args.push('-Dfile.encoding=UTF-8');
-		args.push('-jar');
-		args.push('plugins/org.eclipse.equinox.launcher_1.3.0.v20140415-2008.jar');
-		args.push('-application');
-		args.push('br.com.totvs.tds.cli.tdscli');
-		args.push('-nosplash');
+		this.changeOptions(options);
+
+		let args = [
+			'-Dfile.encoding=UTF-8',
+			'-jar'
+		];
+
+		if (this.version.minor === '2') {
+			args.push(path.join(this.tdsHome, 'tdscli.jar'));
+		}
+		else {
+			args.push(this.launcher);
+			args.push('-application');
+			args.push('br.com.totvs.tds.cli.tdscli');
+			args.push('-nosplash');
+		}
+
 		args.push(target);
 
 		if (options.workspace) {
@@ -98,6 +118,132 @@ class TDS {
 		return args;
 	}
 
+	findTdsHome() {
+		this.tdsHome = process.env.TDS_HOME || process.env.TDS_APPRE || process.cwd();
+
+		if (!process.env.TDS_APPRE) {
+			process.env.TDS_APPRE = this.tdsHome;
+		}
+	}
+
+	findJava() {
+		if (process.env.TDS_HOME) {
+			this.java = path.join(process.env.TDS_HOME, 'jre', 'bin', 'java');
+		}
+		else if (process.env.JAVA_HOME) {
+			this.java = path.join(process.env.JAVA_HOME, 'bin', 'java');
+		}
+
+		if (os.platform() === 'win32') {
+			this.java += '.exe';
+		}
+
+		if (this.java.indexOf(' ') !== -1) {
+			this.java = '"' + this.java + '"';
+		}
+	}
+
+	findVersion() {
+		let command = [
+			this.java,
+			'-jar',
+			this.launcher,
+			'-application',
+			'org.eclipse.equinox.p2.director',
+			'-nosplash',
+			'-listInstalledRoots'
+		].join(' '),
+			out = null,
+			result = null,
+			test = /(?:(?:developerStudio\.full\/)|(?:br\.com\.totvs\.tds\.startup\.product\/))(\d+\.\d+\.\d+)/igm;
+
+		out = execSync(command, { encoding: 'utf8' });
+		result = test.exec(out);
+
+		if (result && result.length > 1) {
+			let v = result[1].split('.');
+
+			this.version = {
+				value: result[1],
+				major_minor: v[0] + '.' + v[1],
+				major: v[0],
+				minor: v[1],
+				patch: v[2]
+			};
+		}
+
+		if (this.version.major_minor === '11.3') {
+			test = /(?:br\.com\.totvs\.tds\.feature\.tdscli\.feature\.group\/)(\d+\.\d+\.\d+)/igm;
+			result = test.exec(out);
+
+			console.log("TDSCLI: " + JSON.stringify(result));
+
+			if (!result || result.length < 2) {
+				console.log("Instaling tdscli...");
+				this.installTdsCli();
+			}
+		}
+	}
+
+	installTdsCli() {
+		let command = [
+			this.java,
+			'-jar',
+			this.launcher,
+			'-application',
+			'org.eclipse.equinox.p2.director',
+			'-nosplash',
+			'-repository',
+			'http://ds.totvs.com/updates/tds' + this.version.major + this.version.minor,
+			'-installIU',
+			'br.com.totvs.tds.feature.tdscli.feature.group'
+		].join(' '),
+			out = null,
+			result = null,
+			test = /(?:(?:developerStudio\.full\/)|(?:br\.com\.totvs\.tds\.startup\.product\/))(\d+\.\d+\.\d+)/igm;
+
+		out = execSync(command, { encoding: 'utf8' });
+		result = test.exec(out);
+
+		//eclipsec.exe -application org.eclipse.equinox.p2.director -repository http://ds.totvs.com/updates/tds113 -nosplash -installIU br.com.totvs.tds.feature.tdscli.feature.group
+		//http://ds.totvs.com/updates/tds113
+	}
+
+	changeOptions(options) {
+		if (this.version.major_minor === '11.2') {
+			if (options.serverType !== undefined) {
+				if (options.serverType.toUpperCase() !== 'ADVPL')
+					options.serverType = '4GL';
+			}
+
+			if (typeof options.recompile === 'boolean') {
+				options.recompile = options.recompile ? 't' : 'f';
+			}
+		}
+		else {
+			if (options.serverType !== undefined) {
+				if (options.serverType.toUpperCase() !== 'ADVPL')
+					options.serverType = 'Logix';
+			}
+
+			if (typeof options.recompile === 'string') {
+				options.recompile = (options.recompile.toLowerCase() === 't');
+			}
+		}
+	}
+
+	findLauncher() {
+		let iniContent = fs.readFileSync(path.join(this.tdsHome, 'developerStudio.ini'), 'utf-8'),
+			test = /(?:-startup\r?\n)(.+)(?:\r?\n)/igm,
+			result = test.exec(iniContent);
+
+		if (result && result.length > 1) {
+			this.launcher = path.join(this.tdsHome, result[1]);
+		}
+		else {
+			this.launcher = '';
+		}
+	}
 }
 
 module.exports = TDS;
