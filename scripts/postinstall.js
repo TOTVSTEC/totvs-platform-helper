9836d8b0f06d80c15ea4da24b00574fdd62be723
Rogerio Ribeiro da Cruz <rogeriorc@gmail.com>
2017-03-14 13:17:51 -0300

    Adicionado script de postinstall para dar chmod +x no appre

---
+++ scripts/postinstall.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+'use strict';
+
+let path = require('path'),
+	fs = require('fs'),
+	execFileSync = require('child_process').execFileSync;
+
+const ARCHITECTURES = ['x86', 'x86_64'];
+
+if (process.platform !== 'win32') {
+	let basedir = path.join(__dirname, '..', 'appre'),
+		os = process.platform === 'darwin' ? 'mac' : 'linux';
+
+	ARCHITECTURES.forEach((arch, index) => {
+		let file = path.join(basedir, arch, os, 'appre');
+
+		if (fs.existsSync(file)) {
+			console.log('chmod +x ' + file);
+
+			execFileSync('chmod', ['+x', file]);
+		}
+	});
+}
